67a8c5f4be1f57017a3767707ae7bece7c3eb49d
Mariana Rodrigues <mariana.s.rodrigues13@aluno.senai.br>
2026-02-06 14:03:43 -0300

    Começando o operador v3

---
+++ v3/operadores-v3.js
@@ -0,0 +1,13 @@
+function Somar(){
+
+};
+
+
+function Limpar(){
+    // esconder o container de resultado
+    document.getElementById('resultado').style.display = 'none';
+
+    // esconder os numeros no input
+    document.getElementById('valor01').value = '';
+    document.getElementById('valor02').value = '';
+};
