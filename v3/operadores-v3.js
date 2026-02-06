1bc0ef940e94932d56938a876f7af887e88342ea
Mariana Rodrigues <mariana.s.rodrigues13@aluno.senai.br>
2026-02-06 14:15:17 -0300

    Fazendo a função somar e limpar no JS

--- v3/operadores-v3.js
+++ v3/operadores-v3.js
@@ -1,4 +1,17 @@
 function Somar(){
+    // obter os elementos e os valores digitados pelos usuários
+    const verificaV1 = document.getElementById('valor01').value;
+    console.log(verificaV1)
+    const verificaV2 = document.getElementById('valor02').value;
+    console.log(verificaV2)
+
+    // IF do JS 
+    
+    // if(teste){
+
+    // } else{
+
+    // };
 
 };
 
